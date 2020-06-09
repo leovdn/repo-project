10842a90dc2e3cc39869efde625a0d1eb71a15b6
leovdn <leo_vdn@hotmail.com>
2020-06-09 00:33:28 -0300

    Built layout and adjusted time interval on script on click

--- scripts/script.js
+++ scripts/script.js
@@ -16,8 +16,7 @@ function showRepos(repos) {
 }
 
 function listRepos() {
-    var smooth = document.getElementById('list');
-    smooth.scrollIntoView({block: "start", behavior: "smooth"});  
+
     const user = inputElement.value;
 
     if(!user) return;
@@ -28,3 +27,29 @@ function listRepos() {
         })
 }
 
+
+function smoothScroll() {
+    var smooth = document.getElementById('list');
+    smooth.scrollIntoView({block: "start", behavior: "smooth"});  
+}
+
+
+function startAnimateScroll() {
+    const sections = document.querySelectorAll('[data-anime="scroll"]');
+
+    if (sections.length) {
+        const windowHalf = window.innerHeight * 0.7;
+
+        function animemateScroll() {
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisible = (sectionTop - windowHalf) < 0;
+                if (isSectionVisible) {
+                    section.classList.add('active');
+                }
+            })
+        }
+        animemateScroll();
+        window.addEventListener('scroll', animemateScroll);
+    }
+}
